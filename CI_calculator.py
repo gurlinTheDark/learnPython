a3255bf1cbdbf3b2fd4592d0c56f3d6dd2afe0d1
ashwinr800 <115934290+ashwinr800@users.noreply.github.com>
2022-10-21 19:03:56 +0530

    Update CI_calculator.py

--- CI_calculator.py
+++ CI_calculator.py
@@ -1,10 +1,12 @@
 sal = {"Ashwin":5000, "Archana":8000, "Asha":7000, "Rajkumar":10000, "Naveen":9000}
 
-for name,salary in sal.items():
-    name = input("Enter your name")
+while (1):
+    name = input("Enter your name ")
+    salary = sal[name]
+    
     if name in sal:
-        r = float(input("Enter your hike per year"))
-        n = int(input("Enter the number of years"))
+        r = float(input("Enter your hike per year "))
+        n = int(input("Enter the number of years "))
         a = salary*(1+(r/100))**(1*n)
         print("%s, after %d years will make Rs %d as salary" %(name,n,a))
     else:
