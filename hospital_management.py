cc6797aa668a2cfcd94bbff3766ef1aaf732096c
ashwinr800 <115934290+ashwinr800@users.noreply.github.com>
2022-11-05 22:46:30 +0530

    Update hospital_management.py

--- hospital_management.py
+++ hospital_management.py
@@ -11,21 +11,30 @@ while (1):
             password = input("Enter the admin passowrd ")
             if password == admin_credentials["password"]:
                 if opn == 1:
-                    data = input("Enter the patient id, name, age and 4 digit aadhar number(4digits) and symptoms ")
+                    data = input("Enter the new patient id, name, age and 4 digit aadhar number(4digits) and symptoms ")
                     data = data.split(",")
-                    if len(data[3]) != 4:
-                        print("Please enter a valid 4 digit aadhar number")
-                            
-                    else:
-                        list_of_patients.append({"id": data[0], 
-                        "name": data[1], 
-                        "age": data[2],
-                        "aadhar number": data[3], 
-                        "symptoms": data[4]
-                        })
-                        no_of_beds+=1
-                        print("The list of patients is %s" %list_of_patients)
-                        print("The number of beds occupied = %d" %no_of_beds)
+                    is_id = False
+                    is_aadhar = False
+                    for i in list_of_patients:
+                        if data[0] == i["id"]:
+                            is_id = True
+                            print("Please enter a new id")
+                        if data[3] == i["aadhar number"]:
+                            is_aadhar = True
+                            print("Please enter a different aadhar number")
+                    if not is_id and not is_aadhar:
+                        if len(data[3]) != 4:
+                            print("Please enter a valid 4 digit aadhar number")
+                        else:
+                            list_of_patients.append({"id": data[0], 
+                            "name": data[1], 
+                            "age": data[2],
+                            "aadhar number": data[3], 
+                            "symptoms": data[4]
+                            })
+                            no_of_beds+=1
+                            print("The list of patients is %s" %list_of_patients)
+                            print("The number of beds occupied = %d" %no_of_beds)
                 if opn == 2:
                     id = input("Enter the patient id you want to delete ")
                     is_id = False
